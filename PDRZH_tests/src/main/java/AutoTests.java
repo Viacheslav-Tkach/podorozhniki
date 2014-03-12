20008f48b4f2b09add3b01279798b72a32fb149e
Viacheslav Tkach <viacheslav_tkach@epam.com>
2014-03-12 16:46:39 +0200

    regCar

--- PDRZH_tests/src/main/java/AutoTests.java
+++ PDRZH_tests/src/main/java/AutoTests.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
+import org.openqa.selenium.NoSuchElementException;
 
 public class AutoTests {
     
@@ -11,7 +12,7 @@ public class AutoTests {
         
         String quer= "select * from car";
         
-        ResultSet result = at.queryExec(quer);
+        //ResultSet result = at.queryExec(quer);
         ArrayList<String> line = new ArrayList();
         /*while(result.next()) { // process results one row at a time
             System.out.println("");
@@ -27,30 +28,26 @@ public class AutoTests {
                 System.out.print(line.get(j)+ "   ");
             }
         }*/
+        
+        String car_color="qwerty";
+        String car_vendor="qwerty";
+        String car_model="qwerty";
+        String car_year="1999";
+        String car_number="1234";
+        String car_seats="2";
         PdrzhLogin pl = new PdrzhLogin();
+        pl.open_start_page();
         pl.login("abc","abc");
-        pl.logout();
-        pl.cleanup();
+        pl.open_edit_profile();
+        pl.open_register_car_page();
+        pl.input_new_car_value(car_color, car_vendor, car_model, car_year, car_number, car_seats);
+        pl.register_car(car_color, car_vendor, car_model, car_year, car_number, car_seats);
+        //pl.logout();
+        //pl.cleanup();
+            
+        }
+
+    
         
-    } 
-        
-     public ResultSet queryExec(String quer) throws SQLException{
-         
-        DBSelector dbs=new DBSelector();
-         
-        String url = "jdbc:postgresql://evbyminsd7238.minsk.epam.com:5432/pdrzh_for_testers";
-        Properties identify  = new Properties();
-        
-        identify.setProperty("user","postgres");
-        identify.setProperty("password","123");
-        
-        Connection conn= dbs.connectToDB(url, identify);
-        
-                
-        ResultSet result=dbs.query(quer, conn);
-        conn.close();
-        return result;
-        
-        
-    }
+     
 }
