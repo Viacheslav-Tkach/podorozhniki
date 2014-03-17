85d598db499c6f22202e30147066a3ecb4ceb595
Viacheslav Tkach <viacheslav_tkach@epam.com>
2014-03-17 17:59:15 +0200

    OMG

---
+++ PdrzhAtests/src/test/java/ui/MainPage.java
@@ -0,0 +1,21 @@
+package ui;
+
+import com.epam.pdrzh.webdriver.core.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
+
+/**
+ * Created by Oleksandr_Rybkin on 14.03.14.
+ */
+public class MainPage {
+    public MainPage(){
+        HtmlElementLoader.populatePageObject(this, Driver.get());
+    }
+    @FindBy(xpath = "//a[.='Edit']")
+    WebElement editButton;
+
+    /*public EditPage enterEditPage(){
+
+    }*/
+}
